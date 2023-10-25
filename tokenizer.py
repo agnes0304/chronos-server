00613d95d79513effc791519142f0675d85681e2
agnes0304 <jiwoochoi0304@gmail.com>
2023-10-26 00:05:39 +0900

    tuned tokenizer ongoing

--- tokenizer.py
+++ tokenizer.py
@@ -28,7 +28,8 @@ for i in range(len(data["words"])):
 okt = Okt()
 tokenized = []
 for i in range(len(words)):
-    tokenized.append(okt.morphs(words[i]))
+    # tokenized.append(okt.morphs(words[i])) -> nouns가 더 나은 결과를 보여주는 것 같음.
+    tokenized.append(okt.nouns(words[i]))
 
 # remove stopwords
 stopwords = ['와','이', '그', '저', '것', '수', '등', '들', '것', '때', '더', '그', '이', '수', '등', '등등', '때문', '때문에', '위', '바로', '좀', '분', '씨', '제', '그것', '이것', '저것','의','에도']
@@ -41,13 +42,16 @@ tokenized = [[j for j in i if j not in punctuation] for i in tokenized]
 # remove numbers
 tokenized = [[j for j in i if not j.isdigit()] for i in tokenized]
 
-# remove duplicated words
-tokenized = [list(set(i)) for i in tokenized]
-
 # remove words with length less than 2
 tokenized = [[j for j in i if len(j) > 1] for i in tokenized]
 
 # remove empty lists
 tokenized = [i for i in tokenized if i != []]
+
+# tokenized 리스트 안에 있는 리스트 항목들 벗겨내기
+tokenized = [item for sublist in tokenized for item in sublist]
+
+# remove duplicated words
+tokenized = list(set(tokenized))
 
 print(tokenized)
