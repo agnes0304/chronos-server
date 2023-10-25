e5031ec9ac199fae21f51ac0e442f23b96df6387
agnes0304 <jiwoochoi0304@gmail.com>
2023-10-26 00:40:00 +0900

    added getting s3 presigned url logic

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 import os
 import psycopg2
+import boto3
 from flask import Flask, request, jsonify
 from dotenv import load_dotenv
+from botocore.exceptions import NoCredentialsError
 
 load_dotenv()
 
@@ -15,8 +17,10 @@ DATABASE_CONFIG = {
     "port": os.getenv("DB_PORT")
 }
 
+
 def get_db_connection():
     return psycopg2.connect(**DATABASE_CONFIG)
+
 
 @app.route('/posts', methods=['GET'])
 def get_posts():
@@ -27,7 +31,8 @@ def get_posts():
 
     # table name: test
     if search_term:
-        cursor.execute("SELECT * FROM test WHERE post_content LIKE %s;", ('%' + search_term + '%',))
+        cursor.execute(
+            "SELECT * FROM test WHERE post_content LIKE %s;", ('%' + search_term + '%',))
     else:
         cursor.execute("SELECT * FROM test;")
 
@@ -37,5 +42,30 @@ def get_posts():
 
     return jsonify(posts)
 
+
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+# s3버킷에 있는 파일 url 생성 -> 프론트에서 클릭하면 해당 링크 갈 수 있게 api endpoint 만들기
+def generate_presigned_url(bucket_name, object_name, expiration=3600):
+    s3_client = boto3.client('s3', aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
+                             aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"),)
+
+    try:
+        response = s3_client.generate_presigned_url('get_object',
+                                                    Params={'Bucket': bucket_name,
+                                                            'Key': object_name},
+                                                    ExpiresIn=expiration)
+    except NoCredentialsError:
+        print('Credentials not available')
+        return None
+
+    return response
+
+
+# Example usage
+bucket = os.getenv("S3_BUCKET")
+object_name = "path/to/your/file.jpg"
+url = generate_presigned_url(bucket, object_name)
+print(url)
