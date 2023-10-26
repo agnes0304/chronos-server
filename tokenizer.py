ead36fe2068bf5ec6606ffec9fa37e1b07119e7d
agnes0304 <jiwoochoi0304@gmail.com>
2023-10-26 15:10:06 +0900

    changed okt to komoran

--- tokenizer.py
+++ tokenizer.py
@@ -2,7 +2,8 @@
 # tokenize the json file using konlpy
 
 import json
-from konlpy.tag import Okt
+# from konlpy.tag import Okt
+from konlpy.tag import Komoran
 
 with open('./data/ocr.json', encoding='utf-8') as data_file:
     data = json.load(data_file)
@@ -21,14 +22,14 @@ for i in range(len(data["words"])):
 ### 7. remove words with length less then 2 -> OK
 
 # tokenize the json file
-okt = Okt()
+okt = Komoran()
 tokenized = []
 for i in range(len(words)):
     # tokenized.append(okt.morphs(words[i])) -> nouns가 더 나은 결과를 보여주는 것 같음.
     tokenized.append(okt.nouns(words[i]))
 
 # remove stopwords
-stopwords = ['와','이', '그', '저', '것', '수', '등', '들', '것', '때', '더', '그', '이', '수', '등', '등등', '때문', '때문에', '위', '바로', '좀', '분', '씨', '제', '그것', '이것', '저것','의','에도']
+stopwords = ['와','이', '그', '저', '것', '수', '등', '들', '것', '때', '더', '그', '이', '수', '등', '등등', '때문', '때문에', '위', '바로', '좀', '분', '씨', '제', '그것', '이것', '저것','의','에도', '위해', '인의']
 tokenized = [[j for j in i if j not in stopwords] for i in tokenized]
 
 # remove punctuation
@@ -39,7 +40,7 @@ tokenized = [[j for j in i if j not in punctuation] for i in tokenized]
 tokenized = [[j for j in i if not j.isdigit()] for i in tokenized]
 
 # remove words with length less than 2
-tokenized = [[j for j in i if len(j) > 1] for i in tokenized]
+# tokenized = [[j for j in i if len(j) > 1] for i in tokenized]
 
 # remove empty lists
 tokenized = [i for i in tokenized if i != []]
@@ -51,3 +52,7 @@ tokenized = [item for sublist in tokenized for item in sublist]
 tokenized = list(set(tokenized))
 
 print(tokenized)
+
+
+# using Komoran
+
