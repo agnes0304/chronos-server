eb8c80249c8740e54889c2aa8c5581ca51ac61c8
agnes0304 <jiwoochoi0304@gmail.com>
2023-10-29 22:39:35 +0900

    improved code

--- tokenizer.py
+++ tokenizer.py
@@ -1,58 +1,53 @@
-# import json file from the data folder
-# tokenize the json file using konlpy
-
 import json
-# from konlpy.tag import Okt
 from konlpy.tag import Komoran
 
-with open('./data/ocr.json', encoding='utf-8') as data_file:
-    data = json.load(data_file)
+# Function to remove unwanted words based on various conditions
+def filter_words(word_list, stopwords, punctuation):
+    return [
+        word for word in word_list 
+        if word not in stopwords 
+        and word not in punctuation 
+        and not word.isdigit()
+        # and len(word) > 1
+    ]
 
-words = []
-for i in range(len(data["words"])):
-    words.append(data["words"][i]["text"])
+# Load JSON data
+with open('./data/ocr.json', 'r', encoding='utf-8') as f:
+    data = json.load(f)
 
-### Tokenize RULEs
-### 1. remove stopwords -> OK
-### 2. remove punctuation -> OK
-### 3. remove numbers -> OK
-### 4. remove english words
-### 5. remove duplicated words
-### 6. remove empty lists -> OK
-### 7. remove words with length less then 2 -> OK
+# Extract text from words in the JSON data
+words = [word_entry["text"] for word_entry in data["words"]]
 
-# tokenize the json file
-okt = Komoran()
-tokenized = []
-for i in range(len(words)):
-    # tokenized.append(okt.morphs(words[i])) -> nouns가 더 나은 결과를 보여주는 것 같음.
-    tokenized.append(okt.nouns(words[i]))
+tokenizer = Komoran()
 
-# remove stopwords
-stopwords = ['와','이', '그', '저', '것', '수', '등', '들', '것', '때', '더', '그', '이', '수', '등', '등등', '때문', '때문에', '위', '바로', '좀', '분', '씨', '제', '그것', '이것', '저것','의','에도', '위해', '인의']
-tokenized = [[j for j in i if j not in stopwords] for i in tokenized]
+stopwords = [
+    '와', '이', '그', '저', '것', '수', '등', '들', '것', 
+    '때', '더', '그', '이', '수', '등', '등등', '때문', 
+    '때문에', '위', '바로', '좀', '분', '씨', '제', '그것', 
+    '이것', '저것', '의', '에도', '위해', '인의'
+]
 
-# remove punctuation
-punctuation = ['.', ',', '!', '?', '(', ')', '[', ']', '{', '}', ':', ';', '-', '_', '+', '=', '/', '*', '~', '`', '@', '#', '$', '%', '^', '&', '|', '\\', '「', '」', '→', '),',')=' ,'《','》','>','·','X','↑→','↑','):','->',')+',':(',"'",'x']
-tokenized = [[j for j in i if j not in punctuation] for i in tokenized]
+punctuation = [
+    '.', ',', '!', '?', '(', ')', '[', ']', '{', '}', ':', 
+    ';', '-', '_', '+', '=', '/', '*', '~', '`', '@', '#', 
+    '$', '%', '^', '&', '|', '\\', '「', '」', '→', '),', 
+    ')=', '《', '》', '>', '·', 'X', '↑→', '↑', '):', '->', 
+    ')+', ':(', "'", 'x'
+]
 
-# remove numbers
-tokenized = [[j for j in i if not j.isdigit()] for i in tokenized]
+# Tokenize and filter
+tokenized = [
+    filter_words(tokenizer.nouns(word), stopwords, punctuation) 
+    for word in words
+]
 
-# remove words with length less than 2
-# tokenized = [[j for j in i if len(j) > 1] for i in tokenized]
+# Remove empty lists
+tokenized = [tokens for tokens in tokenized if tokens]
 
-# remove empty lists
-tokenized = [i for i in tokenized if i != []]
+# Flatten the list of lists
+tokenized = [token for sublist in tokenized for token in sublist]
 
-# tokenized 리스트 안에 있는 리스트 항목들 벗겨내기
-tokenized = [item for sublist in tokenized for item in sublist]
-
-# remove duplicated words
+# Remove duplicate tokens
 tokenized = list(set(tokenized))
 
 print(tokenized)
-
-
-# using Komoran
-
