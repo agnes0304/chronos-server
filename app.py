c3d03b7092e4607f580ef48612f01afcd3c27ea3
agnes0304 <jiwoochoi0304@gmail.com>
2023-10-30 12:41:07 +0900

    added get_public_url()

--- app.py
+++ app.py
@@ -17,9 +17,11 @@ DATABASE_CONFIG = {
     "port": os.getenv("DB_PORT")
 }
 
+
 def get_db_connection():
     """Get a new database connection based on the configuration."""
     return psycopg2.connect(**DATABASE_CONFIG)
+
 
 @app.route('/posts', methods=['GET'])
 def get_posts():
@@ -36,11 +38,19 @@ def get_posts():
 
     return jsonify(posts)
 
+
+def generate_public_url(bucket_name, object_name, region="ap-northeast-2"):
+    base_url = f"https://{bucket_name}.s3.{region}.amazonaws.com"
+    object_url = f"{base_url}/{object_name}"
+    return object_url
+
+
 def generate_presigned_url(bucket_name, object_name, expiration=3600):
-    s3_client = boto3.client('s3', 
-        aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
-        aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY")
-    )
+    s3_client = boto3.client('s3',
+                             aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
+                             aws_secret_access_key=os.getenv(
+                                 "AWS_SECRET_ACCESS_KEY")
+                             )
 
     try:
         params = {'Bucket': bucket_name, 'Key': object_name}
@@ -49,10 +59,17 @@ def generate_presigned_url(bucket_name, object_name, expiration=3600):
         print('Credentials not available')
         return None
 
+
 if __name__ == '__main__':
     app.run(debug=True)
 
 # Example usage
-bucket = os.getenv("S3_BUCKET")
+# public_url = generate_public_url(bucket_name, object_name)
+bucket_name = os.getenv("S3_BUCKET")
 object_name = "path/my/file.jpg"
-url = generate_presigned_url(bucket, object_name)
+url = generate_public_url(bucket_name, object_name)
+
+# presigned_url = generate_presigned_url(bucket_name, object_name)
+bucket_private = os.getenv("S3_PRIVATE_BUCKET")
+object_private = "path/my/file.jpg"
+url = generate_presigned_url(bucket_private, object_private)
