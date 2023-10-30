0087b132aa9278a37cebc2399d3019efbb77b9bd
agnes0304 <jiwoochoi0304@gmail.com>
2023-10-30 15:31:16 +0900

    added insert db query file

---
+++ file_db.py
@@ -0,0 +1,9 @@
+from app import get_db_connection
+
+with get_db_connection() as conn:
+    with conn.cursor() as cursor:
+        cursor.execute(
+            "INSERT INTO files (title, body, filename) VALUES (%s, %s, %s)", ("일제강점기 총정리 요약", "일제강점기 연표 총정리 요약본입니다.", "ver1.5_일제강점기_요약"))
+    conn.commit()
+
+print("insert success")
